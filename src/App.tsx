192493bd53e16f92df0ef9c1eb05308f88bfdce3
Rohit Yadav <rohityadav18450@gmail.com>
2024-05-31 11:29:12 +0530

    implement ton increment

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { TonConnectButton } from "@tonconnect/ui-react";
+import { useMainContract } from "./hooks/useMainContract";
+import { useTonConnect } from "./hooks/useTonConnect";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { contract_address, counter_value, sendIncrement } = useMainContract();
+  const { connected } = useTonConnect();
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <TonConnectButton />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <div className="Card">
+          <b>Our contract Address</b>
+          <div className="Hint">{contract_address?.slice(0, 30) + "..."}</div>
+        </div>
+
+        <div className="Card">
+          <b>Counter Value</b>
+          <div>{counter_value ?? "Loading..."}</div>
+        </div>
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendIncrement();
+            }}
+          >
+            Increment
+          </a>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const manifestUrl =
+  "https://github.com/rohityadav04/tonFrontEnd/blob/main/public/mainifest.json";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </React.StrictMode>,
-)
+  </TonConnectUIProvider>
+);
